1e8835f722e08ab4d7de4aeab900cce875eae59a
Mariana Garcia <maridgarcia045@gmail.com>
2023-08-05 11:38:47 -0300

    fix: add session_start to add elements on the classes array

--- classes.php
+++ classes.php
@@ -1,20 +1,24 @@
 <?php
-    $classes = [];
+    session_start();
+
+    $classes = $_SESSION['classes'] ?? [];
 
     function createClasses() 
     {
         global $classes;
-
+    
         $data = json_decode(file_get_contents("php://input"), true);
-
-        if(empty($data["class_name"]) || empty($data["start_date"]) || empty($data["end_date"]) || empty($data["capacity"])) {
+    
+        if (empty($data["class_name"]) || empty($data["start_date"]) || empty($data["end_date"]) || empty($data["capacity"])) {
             http_response_code(400);
             echo json_encode(["error" => "Check your payload. All fields are required!"]);
             return;
         } 
-        
+    
+        $classes[] = $data;
+        $_SESSION['classes'] = $classes;
+    
         http_response_code(201);
-        $classes = $data;
         echo json_encode($classes);
     }
 ?>
