05ed90aba3254091d8bfb7d5c66f0ab6e1f57e0d
Mariana Garcia <maridgarcia045@gmail.com>
2023-08-06 08:49:02 -0300

    fix: changed success message and added a array to save booked classes

--- booking.php
+++ booking.php
@@ -1,4 +1,5 @@
 <?php
+    $allBookedClasses = $_SESSION['booked_classes'] ?? [];
     function findClassIndex($className, $classes) {
         foreach ($classes as $index => $class) {
             if ($class['class_name'] === $className) {
@@ -25,10 +26,14 @@
         if ($class !== false) {
             $classes[$class]['bookings'] += 1;
 
+            $allBookedClasses[] = $data;
+
             $_SESSION['classes'] = $classes;
+            $_SESSION['booked_classes'] = $allBookedClasses;
 
             http_response_code(200);
             echo json_encode(["message" => "Class booked successfully"]);
+            echo json_encode($allBookedClasses);
         } else {
             http_response_code(404);
             echo json_encode(["error" => "Class not found"]);
